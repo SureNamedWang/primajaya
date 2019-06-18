c5e8efb5c849d34194bb43a8175c9389add82490
SureNamedWang <alphabetasatu@hotmail.com>
2019-06-18 15:59:02 +0700

    tombol+modal edit barang

--- resources/views/barang.blade.php
+++ resources/views/barang.blade.php
@@ -38,26 +38,73 @@
                             <td>
                                 <a href="{{route('updateTipeView', ['id' => $item->id])}}" class="btn btn-block btn-danger"> Update Tipe</a>
                             </td>
-                        </tr>
-                        @endforeach
+                            <td><button type="button" class="btn btn-block btn-primary" data-toggle="modal" data-target="#myModal{{$item->id}}">Edit Barang</button></td>
 
-                        <tr>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                    </tbody>
-                </table>
-                <div class="col-sm-12">
-                    <div class="row">
-                        <div class="col-6">
-                            <a href="{{route('barang.create')}}" class="btn btn-block btn-info">Tambah Barang</a>
+                            <!-- The Modal -->
+                            <div class="modal" id="myModal{{$item->id}}">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <!-- Modal Header -->
+                                        <div class="modal-header">
+                                          <h4 class="modal-title">Approve Pembayaran</h4>
+                                          <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                      </div>
+
+                                      <!-- Modal body -->
+                                      <form method="post" action="#">
+                                        {{method_field('PATCH')}}
+                                        {{csrf_field()}}
+                                        <div class="modal-body">
+                                            <div class="form-group col-sm-12">
+                                                <div class="form-label-group text-center">
+                                                    <img src="{{asset('storage/'.$item->gambarProduct->where('thumbnail', 1)->first()->gambar)}}" style="height: 200px;width: 200px;" />
+                                                </div>
+                                            </div>
+                                            <div class="form-group col-sm-12">
+                                                <div class="form-label-group">
+                                                    <input type="text" class="w-100" name="nama" value="{{$item->nama}}" required>
+                                                    <label for="nama">Nama</label>
+                                                </div>
+                                            </div>
+                                            <div class="form-group col-sm-12">
+                                                <div class="form-label-group">
+                                                    <h6>Detail</h6>
+                                                    <textarea name="detail" class="w-100" cols="25" rows="5">{{$item->detail}}</textarea>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <!-- Modal footer -->
+                                        <div class="modal-footer">
+                                            <input type="submit" name="submit" class="btn btn-info" value="Update"></button>
+
+                                            <button type="button" class="btn btn-danger" data-dismiss="modal">Batal</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
                         </div>
+                        <!-- /endModal -->
+                    </tr>
+                    @endforeach
+
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+            <div class="col-sm-12">
+                <div class="row">
+                    <div class="col-6">
+                        <a href="{{route('barang.create')}}" class="btn btn-block btn-info">Tambah Barang</a>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
 </div>
 <script type="text/javascript">
 
