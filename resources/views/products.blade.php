4db1877053d4a80e717aab3466122ff3203f1779
SureNamedWang <alphabetasatu@hotmail.com>
2019-06-23 19:15:06 +0700

    update template

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,93 +1,48 @@
 @extends('index')
 @section('content')
-<!-- Breadcrumbs-->
-<ol class="breadcrumb">
-	<li class="breadcrumb-item">
-		<a href="#">Index</a>
-	</li>
-	<li class="breadcrumb-item active">Products</li>
-</ol>
+
 <!-- #Category 1 -->
 <div class="card mb-3">
-	<div class="card-header" style="font-weight: bold; font-size: 25px ;background-color: beige">
-		TENDA
-	</div>
 	<div class="card-body">
 		<div class="row">
 			@foreach ($products as $product)
 
-			<div class="col-xl-4 col-sm-6 mb-3">
+			<div class="col-xl-4 col-sm-12 mb-3">
 				<a href="{{route('catalogue.show', ['id' => $product->id])}}">
-					<div class="w3-container">
-							<h6 style="font-weight: bolder">{{ucwords($product->nama)}}</h6>
-							<div class="w3-card-4">
-								<img src="{{asset('storage/'.$product->gambarProduct->where('thumbnail', 1)->first()->gambar)}}" alt="Norway" style="width:100%;height: 200px">
-								<div class="w3-container w3-center" style="height: 80px">
-									<?php
-									echo (substr($product->detail,0,75).'...');
-									?>								
+					<div class="card card-post card-round">
+						<img class="card-img-top" 
+						src="{{asset('storage/'.$product->gambarProduct->where('thumbnail', 1)->first()->gambar)}}"
+						alt="Card image cap"
+						style="height: 200px;object-fit: cover">
+						<div class="card-body">
+							<div class="d-flex">
+								<div class="info-post ml-2">
+									<p class="username">{{ucwords($product->nama)}}</p>
+									<p class="date text-muted">Rp.
+										{{ number_format($product->hargaUkuranProduct->first()->harga,0,',','.')}}
+									</p>
 								</div>
-								<footer class="w3-container w3-red">
-									<h6>Start from:</h6>
-									<h5 class="w3-center">Rp. {{ number_format($product->hargaUkuranProduct->first()->harga,0,',','.')}}</h5>
-								</footer>
 							</div>
-							<input type="hidden" name="idBarang" value="{{$product->id}}">
-						</div>
-					</a>
-				</div>
-				
-				@endforeach
-			</div>
-			<!-- /#row -->
-		</div>
-		<!-- /#card body -->
-	</div>
-	<!-- /#card -->
-	<!-- /#Category 1 -->
-
-	<!-- #Category 2 -->
-	<div class="card mb-3">
-		<div class="card-header" style="font-weight: bold; font-size: 25px ;background-color: lightcyan">
-			NEON
-		</div>
-		<div class="card-body">
-			<div class="row">
-				<div class="col-xl-4 col-sm-6 mb-3">
-					<div class="w3-container">
-						<h2>Photo Card</h2>
-						<div class="w3-card-4">
-							<img src="la.jpg" alt="Norway" style="width:100%">
-							<div class="w3-container w3-center">
-								<p>The Italian / Austrian Alps</p>
-							</div>
-							<footer class="w3-container w3-red">
-								<h5>Footer</h5>
-							</footer>
+							<div class="separator-solid"></div>
+							<p class="card-text">
+								<?php
+									echo (substr($product->detail,0,75).'...');
+								?>
+							</p>
+							<a href="{{route('catalogue.show', ['id' => $product->id])}}"
+								class="btn btn-primary btn-rounded btn-sm mt-2 float-right">Read More</a>
 						</div>
 					</div>
-				</div>
-				<div class="col-xl-4 col-sm-6 mb-3">
-					<div class="w3-container">
-						<h2>Photo Card</h2>
-
-						<div class="w3-card-4">
-							<img src="la.jpg" alt="Norway" style="width:100%">
-							<div class="w3-container w3-center">
-								<p>The Italian / Austrian Alps</p>
-							</div>
-							<footer class="w3-container w3-red">
-								<h5>Footer</h5>
-							</footer>
-						</div>
-					</div>
-				</div>
+				</a>
 			</div>
-			<!-- /#row -->
-		</div>
-		<!-- /#card body -->
-	</div>
-	<!-- /#card -->
-	<!-- /#Category 2 -->
 
-	@endsection
+			@endforeach
+		</div>
+		<!-- /#row -->
+	</div>
+	<!-- /#card body -->
+</div>
+<!-- /#card -->
+<!-- /#Category 1 -->
+
+@endsection
